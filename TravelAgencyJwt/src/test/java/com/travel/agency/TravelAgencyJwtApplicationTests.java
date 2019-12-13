04654d29c1cf80876ccf679e3f39476ca85ffbb4
shanth5100 <prashanth.veeraboena@gmail.com>
2019-12-13 11:03:22 +0530

    0.1: project Added

---
+++ TravelAgencyJwt/src/test/java/com/travel/agency/TravelAgencyJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
