5b6da0bc91cae486ae892f763e057c1a8bbd1e3c
shanth5100 <prashanth.veeraboena@gmail.com>
2019-12-24 19:43:47 +0530

    0.1: For login error with lokeduser

--- TravelAgencyJwt/src/main/java/com/travel/agency/config/security/SecurityConfig.java
+++ TravelAgencyJwt/src/main/java/com/travel/agency/config/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.travel.agency.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -11,18 +10,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.travel.agency.config.security.jwt.JwtAuthenticationEntryPoint;
 import com.travel.agency.config.security.jwt.JwtAuthenticationFilter;
-
+//http:www.appsdeveloperblog.com/user-authentication-spring-boot-spring-security-jwt/
+import com.travel.agency.config.security.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	CustomUserDetailsService customUserDetailsService;
 	
 	 @Autowired
 	 private JwtAuthenticationEntryPoint unauthorizedHandler;
@@ -35,23 +39,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService())
+        auth.userDetailsService(customUserDetailsService)
                 .passwordEncoder(passwordEncoder());
+//        auth.userDetailsService(userDetailsService())
+//        .passwordEncoder(passwordEncoder());
     }
+	
+	@Bean
+	 @Override
+	protected UserDetailsService userDetailsService() {
+		return super.userDetailsService();
+	}
 
 	@Bean
     public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
     }
-	
-//	@Override some times get the Error .hasRole("user")
-//    public void configure(WebSecurity web) { // ignoring any request 
-////		web.ignoring().antMatchers("/address/");
-//		web.ignoring().antMatchers("/oauth/user/register");
-//	}
+
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
+		System.out.println("configure(HttpSecurity http)");
 //		http
 //			.authorizeRequests()
 //			.antMatchers("/oauth/user/register").permitAll()
@@ -81,14 +89,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                  "/**/*.css",
                  "/**/*.js")
                  .permitAll()
-//             .antMatchers("/api/auth/**")
-//                 .permitAll()
-             .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
-                 .permitAll()
-             .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**")
+                 .antMatchers("/api/auth/**")
+//                 .antMatchers("/api/auth/register") // to allow only registration 
                  .permitAll()
              .anyRequest()
-             .permitAll();
+             .authenticated()
+//             .and() // from here Adding custom filter for Accessing the Data
+             ;
          
 		 // Add our custom JWT security filter
 	      	http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
@@ -96,7 +103,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	JwtAuthenticationFilter jwtAuthenticationFilter() { // Should not be private, final 
+		System.out.println("JwtAuthenticationFilter jwtAuthenticationFilter");
 		return new JwtAuthenticationFilter();
 	}
+	
+	
+//	@Override some times get the Error .hasRole("user")
+//    public void configure(WebSecurity web) { // ignoring any request 
+////		web.ignoring().antMatchers("/address/");
+//		web.ignoring().antMatchers("/oauth/user/register");
+//	}
 	
 }
